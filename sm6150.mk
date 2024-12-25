8f42264300402373de51eedc4aca701ca9f21450
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    sm6150-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- sm6150.mk
+++ sm6150.mk
@@ -257,6 +257,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
+PRODUCT_PACKAGES += \
+     NcmTetheringOverlay
+
 # Partitions
 PRODUCT_PACKAGES += \
     vendor_bt_firmware_mountpoint \
