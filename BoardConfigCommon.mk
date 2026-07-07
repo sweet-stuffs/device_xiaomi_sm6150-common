4771b85ed974f00e17667eb5101814b76300f784
K R I S H N A <krishnakripa34567@gmail.com>
2026-07-07 12:03:41 +0000

    Revert "sm6150-common: Drop TARGET_KERNEL_CONFIG"
    This reverts commit 24a6bf15f82a274b4f635f7179696bfae6be8b03.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,6 +69,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 TARGET_KERNEL_CLANG_VERSION := r563880c
 TARGET_KERNEL_NO_GCC := true
+TARGET_KERNEL_CONFIG := vendor/sdmsteppe-perf_defconfig
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
